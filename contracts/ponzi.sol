83d5d7fc79c477283d23abe9da0502248de724ef
Felice Pollano <felice.pollano@gmail.com>
2018-05-12 12:33:15 +0200

    added function to invite friends for free

--- contracts/ponzi.sol
+++ contracts/ponzi.sol
@@ -7,6 +7,7 @@ contract Ponzi {
     mapping(address => uint) public presentedCount;
     constructor () public {
         owner = msg.sender;
+        members[owner] = true;
         price = 10 finney; //milliether 
     } 
     //amount is in wei
@@ -19,13 +20,25 @@ contract Ponzi {
         }
         return false;
     }
+    //this function allow to invite friends or developers
+    //without any fee
+    function inviteFriends(address friend) public returns (bool){
+        if( msg.sender == owner ){
+            //friend invitation for free  does not deserve any money back
+            members[friend] = true;
+            return true;
+        }
+        return false;
+    }
+    //call this function to join the Ponzi's pyramid
+    //presentedby is the address of whom invited you
     function join(address presentedby) public payable returns(bool) {
         if(msg.value==price){
             //presenter must be a member of the Ponzi's pyramid
             if( members[presentedby]==true ){
                 members[msg.sender]==true; // the presented guy join the ponzy's schema
-                if( presentedCount[presentedby]%2 == 0){
-                    //reward member each couple of prresented new members!
+                if( presentedCount[presentedby] >0 && presentedCount[presentedby]%2 == 0){
+                    //reward member each couple of presented new members!
                     presentedby.transfer(price);
                     return true;
                 }
